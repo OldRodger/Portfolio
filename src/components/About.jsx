63cb2e308a96965771e49a113c04d53bcbd07e75
Celestine <nwachukwucelestine234@gmail.com>
2024-01-10 10:50:33 +0000

    fixed padding on section heading

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
     return (
         <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full'>
-                <div className='pb-8'>
+                <div className='pt-8 md:pb-8 md:pt-0'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
                 <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sint at eos omnis reprehenderit ipsum aperiam. A alias dolores facere vel accusantium ipsa aut voluptatum adipisci quisquam ducimus iste quis maiores ea similique molestias odit officiis, tenetur velit distinctio impedit! Obcaecati consequatur quis quae rerum aliquid omnis reiciendis aperiam accusamus!</p>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -51,7 +51,7 @@ function Portfolio() {
     return (
         <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-                <div className='pb-8'>
+                <div className='pt-10 md:pb-8 md:pt-0'>
                     <p className='text-4xl font-bold border-b-4 border-gray-500 inline-block'>Portfolio</p>
                     <p className='py-6'>Check out some of my work right here</p>
                 </div>
